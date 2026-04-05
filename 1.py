44e36815e3fce0f6c8e293040c7f6b60c80f91d6
pixelblink <chandandhare98@gmail.com>
2026-04-06 03:43:00 +0530

    day1

---
+++ 1.py
@@ -0,0 +1,21 @@
+# eval()
+# eval uses the given data and evaluates its datatype by itself
+
+# x=eval(input("enter any data:- "))
+# print(x, type(x))
+
+'''s=input("enter your name:- ")
+
+# print function
+print(s)
+print(type(s))
+
+# print using f string
+print(f"type of {s} is {type(s)}'''
+
+# ord() function is used to find the ascii value of a string
+print(ord("a"))
+
+# chr() function is used to find the letter from the number
+print(chr(97))
+

b2ec3c7b2e7901631ea133dacddd298bc3285912
pixelblink <chandandhare98@gmail.com>
2026-04-08 00:13:21 +0530

    day3

---
+++ 3.py
@@ -0,0 +1,37 @@
+# methods
+# Split()
+
+# s="python"
+
+# print(s.split())
+# print(s.split('p'))
+# print(s.split(' ',2))
+# print(s.split(' ',0))
+
+
+# Join()
+
+# s1='python'
+# s2='java'
+# s3='php'
+
+# x=','.join(s1,s2,s3) #wouldnt work, because join takes only one argument 
+
+# x=','.join([s1,s2,s3]) #so we have to give the single argument as a list
+# print(x)
+
+
+# Strip() it strips the user of his cursed energy
+
+# s='!!!!python!!!!'
+# print(s.strip('!'))
+
+# s='!!!!python***'
+# print(s.strip('!*'))
+
+
+# startswith()
+
+s='python'
+print(s.startswith('py'))
+

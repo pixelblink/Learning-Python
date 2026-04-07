e8b35184ea43734467ae429730fb852a4efc1a89
pixelblink <chandandhare98@gmail.com>
2026-04-07 12:57:17 +0530

    day2

---
+++ 2.py
@@ -0,0 +1,31 @@
+# string methods
+
+'''lower()
+upper()
+title()
+startswith()
+endswith()
+split()
+join()
+index()
+join()
+capitalise()'''
+
+
+# syntax
+
+# s=input("enter string")
+
+# print(s.lower())
+# print(s.upper())
+# print(s.title())
+# print(s.capitalise())
+
+# ch = input("enter any character")
+s=input("enter name:-")
+ch=input("enter any character=")
+start=int(input("enter start point:-"))
+stop=int(input("enter stop point:-"))
+print(s.index(ch,start,stop))
+
+# Q. 

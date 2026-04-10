91ed0c624f7e11872257f0ccac7f97fbede04ae6
pixelblink <chandandhare98@gmail.com>
2026-04-10 12:46:49 +0530

    day5

---
+++ 5.py
@@ -0,0 +1,79 @@
+# list methods
+
+# copy() create new elements with some elements
+# clear() clear all elements from given list
+# append() add single element at last position
+# extend() add multiple element at last position
+# insert() add single element at targeted indexing
+# pop()  remove single element at targeted indexing
+# remove() remove element
+# sort() arrange in ascending order
+# reverse() reverse out all index position
+
+
+# # append()
+
+# l=eval(input("enter any list: "))
+# element=eval(input("enter element: "))
+
+# l.append(element)
+# print(l)
+
+
+# # extend()
+
+# l=eval(input("enter any list: "))
+# element=eval(input("enter element: "))
+
+# l.extend(element)
+# print(l)
+
+# # position()
+
+# l=eval(input("enter any list: "))
+# element=eval(input("enter element: "))
+# position=
+# l.extend(position, element)
+# print(l)
+
+
+
+# pop()
+
+# l=[1,2,3,4,5,6]
+# l.pop() # removes last element without inxeding
+# print(l)
+
+# l.pop(1) # with indexing removes targetted element
+# print(l)
+
+
+# remove()
+
+# l=[1,2,3,4,5,6]
+# l.remove(2) #not indexing but the element is directly targetted as element
+# print(l)
+
+# index()
+
+# l=[1,2,3,4,5,6]
+# print(l.index(2)) #to find index of an element
+
+# # count()
+
+# l=[1,2,3,2,2,6]
+# print(l.count(2)) #to find occurance of an element
+
+
+# sort()
+
+l=[1,2,3,2,2,6]
+l.sort() #sorting list into ascending order
+print(l)
+
+# reverse()
+
+l=[1,2,3,2,2,6]
+l.reverse() #reversing list into descending order
+print(l)
+

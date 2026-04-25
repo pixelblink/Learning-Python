26d9e7f1bc93005c8f819be15bcc7777f4b5cd61
pixelblink <chandandhare98@gmail.com>
2026-04-26 02:45:58 +0530

    day13

---
+++ 13.py
@@ -0,0 +1,78 @@
+# FOR LOOP on iterables
+
+# l=[10,20,30,40,50]
+# l1=[]
+
+# for i in l:
+#     l1.append(i+5)
+# print(l1)
+
+
+# l=[10,20,30,40,50]
+# t=(1,2,3,4,5,6) 
+# l=list(l)
+# l1=[]
+# for i in l:
+#     l1.append(i+5)
+# t=tuple(l1)
+# print(t)
+# print(l)
+
+
+# l=[1,2,3,4,5]
+# for i in range(len(l)):
+#     x=l[i]+5
+#     l[i]=x
+# print(l)
+
+
+# d={'x':10,'y':100,'z':1000}
+# for i in d:
+#     print(i, "=",d[i])
+
+# for i in d:
+#     print(d[i])
+
+# for i in d.keys():
+#     print(i)
+
+# for i in d.values():
+#     print(i)
+
+
+# s={1,2,3,"hello", "yes"}
+# for i in s:
+#     print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

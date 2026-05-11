a987a895557d08e05c10e1928e846a92b0be1ffa
pixelblink <chandandhare98@gmail.com>
2026-05-12 00:59:05 +0530

    day18

---
+++ 18.py
@@ -0,0 +1,36 @@
+# Variable Scope
+# local, global and non-local
+
+# if True:
+#     x=10
+#     print(x)
+# print(x)
+
+
+# understanding errors
+# x=20
+# def add():
+#     print(x)
+#     x=10 #local variable inside of a function
+#     print(x)
+# add()
+# print(x)
+
+
+# underrstand global keyword
+# def add():
+#     global x
+#     x=10
+#     print(x)
+# add()
+# print(x)
+
+x=20
+def add():
+    x=10
+    print(x)
+    print(globals()['x']) #to access global variable to local
+add()
+print(x)
+
+
